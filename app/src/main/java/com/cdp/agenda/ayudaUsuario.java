2cf003bfe718882248290dd8b431632006422415
maria <maria.calani.uvaldez@gmail.com>
2022-05-25 20:22:31 -0400

    videos en forma vertical

--- app/src/main/java/com/cdp/agenda/ayudaUsuario.java
+++ app/src/main/java/com/cdp/agenda/ayudaUsuario.java
@@ -2,6 +2,7 @@ package com.cdp.agenda;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.widget.MediaController;
@@ -13,6 +14,7 @@ public class ayudaUsuario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ayuda_usuario);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         VideoView mivdeo= findViewById(R.id.videoUsuario);
 
